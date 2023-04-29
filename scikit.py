c90cc5a28c53b0d247f0ff05952e0d4a4687e041
frantxu30 <fragonzcuriel@gmail.com>
2023-04-29 17:22:36 +0200

    imprime el documento y a que topico pertenece

--- scikit.py
+++ scikit.py
@@ -3,23 +3,41 @@ import numpy as np
 from sklearn.decomposition import LatentDirichletAllocation
 import sys
 import pandas as pd
+import numpy as np
+
 dataset_name = sys.argv[1]
 df = pd.read_csv(dataset_name)
-X= df['text']
-#aplicamos tf-idf en la columna text
+X = df['text']
+
+# aplicamos tf-idf en la columna text
 vectorizer = CountVectorizer()
 tf_matrix = vectorizer.fit_transform(X)
-#creamos un elemnto LatentDirichAtLocation con el número de tópicos = 5
-lda_model = LatentDirichletAllocation(n_components=5)
+
+# creamos un elemento LatentDirichAtLocation con el número de tópicos = 10
+lda_model = LatentDirichletAllocation(n_components=10)
 lda_model.fit(tf_matrix)
+
+resultados = []
+topicos = {}
+indice = 0
 for j in tf_matrix:
     doc_lda = lda_model.transform(j)
-    print(doc_lda)
-
+    index = doc_lda.argmax()
+    resultados.append(index)
     topic_words = []
     for i, topic in enumerate(lda_model.components_):
         top_words_idx = np.argsort(topic)[::-1][:10] # obtenemos las 10 palabras más relevantes
-        topic_words.append([vectorizer.get_feature_names()[idx] for idx in top_words_idx])
-    # imprime las 10 palabras más relevantes para cada tópico
-    print(topic_words)
+        topic_words.append([vectorizer.get_feature_names_out()[idx] for idx in top_words_idx])
+    indice =indice +1
+    if indice ==20:
+        break
+i = 0
+print(len(resultados))
+for documento in X:
+    print('El documento: ' + str(documento))
+    print('pertenece al tópico: ' + str(resultados[i]))
+    i += 1
+    if(i == 20):
+        break
+
 
